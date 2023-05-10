b05e61d094e3d620c9898cc1a290d08e5e1fc542
alicia8989 <114919948+alicia8989@users.noreply.github.com>
2023-05-10 12:57:11 +0200

    Create par_impar.py

---
+++ par_impar.py
@@ -0,0 +1,11 @@
+def par_impar(num):
+    if(num % 2 == 0):
+        return "par"
+    else:
+        return "impar"
+    
+print (par_impar(5))
+print (par_impar(8))
+
+numero = int(input("Dime un número"))
+print(par_impar(num))
