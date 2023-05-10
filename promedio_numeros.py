37eab01f3452626e240a030bbfa6b83e766c0a49
alicia8989 <114919948+alicia8989@users.noreply.github.com>
2023-05-10 12:56:15 +0200

    Create promedio_numeros.py

---
+++ promedio_numeros.py
@@ -0,0 +1,9 @@
+def numeros(num_1,num_2,num_3):
+    promedio = (num_1 + num_2 + num_3) / 3
+    return promedio
+
+num1 = int(input("Dime un numero:"))
+num2 = int(input("Dime un numero:"))
+num3 = int(input("Dime un numero:"))
+
+print(numeros(num1,num2,num3))
